2afff6cb26f4d54d0330ebe6cc4f8492814ec33b
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-03-03 20:14:04 +0100

    Update ratings views to enable GET for everyone and CRUD for owner

--- ratings/views.py
+++ ratings/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly # noqa
 from hackcollect.permissions import IsOwnerOrReadOnly
 from .models import Rating
 from .serializers import RatingSerializer
@@ -8,10 +8,17 @@ from .serializers import RatingSerializer
 class RatingListCreateView(generics.ListCreateAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class RatingRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly, IsAuthenticated]
+
+    def perform_destroy(self, instance):
+        if instance.owner == self.request.user:
+            instance.delete()
