9177d210b87036f005b3a1ff60b5a5e2aea2f5c0
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-03-03 09:13:38 +0100

    Add profile serializer

---
+++ profiles/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializers(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    # Calls the context to determine if the user is the profile's owner
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id',
+            'owner',
+            'created_at',
+            'updated_at',
+            'name',
+            'content',
+            'image',
+            'is_owner',
+        ]
