75c42f19c5b1db7fa73b4692d59263b08e0731fa
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-03-03 20:16:43 +0100

    Update ruleset for CRUD on ratings views

--- ratings/views.py
+++ ratings/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly # noqa
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from hackcollect.permissions import IsOwnerOrReadOnly
 from .models import Rating
 from .serializers import RatingSerializer
@@ -11,14 +11,13 @@ class RatingListCreateView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        if self.request.user.is_authenticated:
+            serializer.save(owner=self.request.user)
+        else:
+            raise PermissionError("Only registered users can leave a rating!")
 
 
 class RatingRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
-    permission_classes = [IsOwnerOrReadOnly, IsAuthenticated]
-
-    def perform_destroy(self, instance):
-        if instance.owner == self.request.user:
-            instance.delete()
+    permission_classes = [IsOwnerOrReadOnly]
