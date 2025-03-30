36a35d6b5950bfd868192d172bc6b163625fb5fc
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-03-30 22:24:06 +0200

    Add average rating to HackList

--- hacks/views.py
+++ hacks/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics, permissions
 from hackcollect.permissions import IsOwnerOrReadOnly
 from .models import Hack
+from django.db.models import Avg
 from .serializers import HackSerializer
 
 
 class HackList(generics.ListCreateAPIView):
     serializer_class = HackSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Hack.objects.all().order_by('-created_at')
+    queryset = Hack.objects.all().annotate(
+        average_rating=Avg('rating__rating')
+    ).order_by('-created_at')
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
