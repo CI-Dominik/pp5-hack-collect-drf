702a4b2a4edf5dc53ca8fe6c5e40b6b839c07c77
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-03-30 23:41:29 +0200

    Change display of average rating to FloatField

--- hacks/views.py
+++ hacks/views.py
@@ -4,13 +4,16 @@ from .models import Hack
 from django.db.models import Avg
 from django.db.models.functions import Coalesce
 from .serializers import HackSerializer
+from django.db.models import FloatField
 
 
 class HackList(generics.ListCreateAPIView):
     serializer_class = HackSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Hack.objects.all().annotate(
-        average_rating=Coalesce(Avg('rating__rating'), 0)
+        average_rating=Coalesce(
+            Avg('rating__rating'), 0, output_field=FloatField()
+            )
     ).order_by('-created_at')
 
     def perform_create(self, serializer):
@@ -21,5 +24,7 @@ class HackDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = HackSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Hack.objects.all().annotate(
-        average_rating=Coalesce(Avg('rating__rating'), 0)
+        average_rating=Coalesce(
+            Avg('rating__rating'), 0, output_field=FloatField()
+            )
     ).order_by('-created_at')
