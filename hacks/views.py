f3c8679144710b35bdab674b2321cda97fba20e2
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-04-06 22:37:06 +0200

    Add category ordering to fields and comments_count to annotation for hacks

--- hacks/views.py
+++ hacks/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions, filters
 from hackcollect.permissions import IsOwnerOrReadOnly
 from .models import Hack
-from django.db.models import Avg
+from django.db.models import Avg, Count
 from django.db.models.functions import Coalesce
 from .serializers import HackSerializer
 from django.db.models import FloatField
@@ -13,7 +13,8 @@ class HackList(generics.ListCreateAPIView):
     queryset = Hack.objects.all().annotate(
         average_rating=Coalesce(
             Avg('rating__rating'), 0, output_field=FloatField()
-            )
+            ),
+        comments_count=Count('comments', distinct=True)
     ).order_by('-created_at')
 
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
@@ -24,6 +25,7 @@ class HackList(generics.ListCreateAPIView):
 
     ordering_fields = [
         'average_rating',
+        'category'
     ]
 
     def perform_create(self, serializer):
