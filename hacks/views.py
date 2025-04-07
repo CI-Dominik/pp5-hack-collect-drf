30d9e4b561482fbce40af1abbaa76352151bc54a
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-04-07 02:55:37 +0200

    Adjust filterset for Hacks

--- hacks/views.py
+++ hacks/views.py
@@ -19,7 +19,7 @@ class HackList(generics.ListCreateAPIView):
 
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = [
-        'owner__rating__owner',
+        'rating__owner',
     ]
     search_fields = [
         'owner__username',
