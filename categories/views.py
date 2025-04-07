893fecf682763be54424156dcc24b3410dd239de
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-04-07 12:33:01 +0200

    Update category permissions for only admins

--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -7,8 +8,18 @@ class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    permission_classes = [IsAdminUser]
+
+    def perform_create(self, serializer):
+        if self.request.user.is_staff:
+            serializer.save()
+        else:
+            raise PermissionError("Only admins can create a category.")
+
 
 class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     lookup_field = 'pk'
+
+    permission_classes = [IsAdminUser]
