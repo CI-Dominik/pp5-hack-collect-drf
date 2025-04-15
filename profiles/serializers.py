5df258a471f00153155c98cde73ace17f064542b
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-04-15 03:52:14 +0200

    Edit profile views and serializer for following

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,26 +1,35 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    hacks_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     # Calls the context to determine if the user is the profile's owner
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
 
     class Meta:
         model = Profile
         fields = [
-            'id',
-            'owner',
-            'created_at',
-            'updated_at',
-            'name',
-            'content',
-            'image',
-            'is_owner',
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'content', 'image', 'is_owner', 'following_id',
+            'hacks_count', 'followers_count', 'following_count',
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,40 @@
-from rest_framework import generics
+from rest_framework import generics, filters
+from django.db.models import Count
 from hackcollect.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all().order_by('-created_at')
+    queryset = Profile.objects.annotate(
+        hacks_count=Count('owner__hack', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
+    ]
+    ordering_fields = [
+        'hacks_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all().order_by('-created_at')
+    queryset = Profile.objects.annotate(
+        hacks_count=Count('owner__hack', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
