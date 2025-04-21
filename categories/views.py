e55c8274b5a7fa92a57de85b7ab7210f4efb5ef1
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-04-21 17:03:29 +0200

    Change permission class for categories

--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics, status
-from rest_framework.permissions import IsAdminUser
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework.response import Response
 from .models import Category
@@ -26,7 +25,7 @@ class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     lookup_field = 'pk'
-    permission_classes = [IsAdminUser]
+    permission_classes = [ReadOnlyOrAdmin]
 
     def destroy(self, request, *args, **kwargs):
         if Category.objects.count() <= 1:
