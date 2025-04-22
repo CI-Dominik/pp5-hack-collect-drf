ca9f2c6642109573fe4a22d33d0127ba08dd5bfb
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-04-22 14:05:14 +0200

    Add image validation to profile picture

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -11,12 +11,31 @@ class ProfileSerializer(serializers.ModelSerializer):
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
 
+    # Validate image size and dimensions
+
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size is larger than 2 '
+                                              'megabytes, please choose a '
+                                              'smaller image!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height is larger than 4096 pixels, '
+                'please choose a smaller image!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width is larger than 4096 pixels, '
+                'please choose a smaller image!'
+            )
+        return value
+
     # Calls the context to determine if the user is the profile's owner
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-    
+
     def get_following_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
