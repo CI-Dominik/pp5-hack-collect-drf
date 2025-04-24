fc43426f88158f76ffa655ae2910242dec0ef214
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-04-24 17:42:49 +0200

    Set debug to False

--- hackcollect/settings.py
+++ hackcollect/settings.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG")
+DEBUG = False
 
 # REST Framework setup
 
